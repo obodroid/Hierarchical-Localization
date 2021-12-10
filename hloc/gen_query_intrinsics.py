756c4409707e7d59f158cc9d838432d2de6a4f9c
Jiradej Vatanutanon <jiradej.v@obodroid.com>
2021-12-10 16:04:37 +0700

    support generating nested queries with intrinsics

--- hloc/gen_query_intrinsics.py
+++ hloc/gen_query_intrinsics.py
@@ -1,22 +1,17 @@
 import os
 from pathlib import Path
-#path_query = Path('querySTNight')
-#dbinfile = Path('outputs/sfm5/SFMrecons/db_with_intrinsics_m0.txt')
-#outputpath = Path('outputs/sfm5/SFMLocSTnightretq5')
-#outputname = "queries_with_intrinsics.txt"
 
 
 def query_intrinsics(img_path, db_intrinsics_file, output_path, output_name):
-    print('Creating lists of quries intrinsics...')
-    file_list = os.listdir(img_path)
+    print('creating list of queries with intrinsics...')
     with open(db_intrinsics_file) as intfile:
         intrins_param = intfile.readline().rstrip()
         intfile.close()
     with open(output_path / output_name, 'w') as f:
-        for i in file_list:
-            f.write(i + ' ' + intrins_param + '\n')
+        for root, dirs, files in os.walk(img_path):
+            rel_dir = Path(os.path.relpath(root, img_path))
+            for filename in files:
+                f.write(str(rel_dir / filename) + ' ' + intrins_param + '\n')
         f.close()
-        print('a file has been saved in ' + "'" +
+        print('queries with intrinsics file has been saved in ' +
               str(output_path) + '/' + str(output_name) + "'.")
-
-# query_intrinsics(path_query,dbinfile,outputpath,outputname)
