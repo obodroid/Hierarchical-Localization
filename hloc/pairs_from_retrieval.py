02ce3e1a509f1982bafe46954e398243c3dea919
Jiradej Vatanutanon <jiradej.v@obodroid.com>
2022-01-11 16:32:40 +0700

    update pairs from retrieval to match original

--- hloc/pairs_from_retrieval.py
+++ hloc/pairs_from_retrieval.py
@@ -4,6 +4,7 @@ from typing import Optional
 import h5py
 import numpy as np
 import torch
+import collections.abc as collections
 
 from . import logger
 from .utils.parsers import parse_image_lists
@@ -67,38 +68,25 @@ def main(descriptors, output, num_matched,
          db_prefix=None, db_list=None, db_model=None, db_descriptors=None):
     logger.info('Extracting image pairs from a retrieval database.')
 
-    h5_names = []
-    hfile.visititems(
-        lambda _, obj: h5_names.append(obj.parent.name.strip('/'))
-        if isinstance(obj, h5py.Dataset) else None)
-    h5_names = list(set(h5_names))
+    # We handle multiple reference feature files.
+    # We only assume that names are unique among them and map names to files.
+    if db_descriptors is None:
+        db_descriptors = descriptors
+    if isinstance(db_descriptors, (Path, str)):
+        db_descriptors = [db_descriptors]
+    name2db = {n: i for i, p in enumerate(db_descriptors)
+               for n in list_h5_names(p)}
+    db_names_h5 = list(name2db.keys())
+    query_names_h5 = list_h5_names(descriptors)
 
-    if db_prefix or db_prefix == "":
-        if not isinstance(db_prefix, str):
-            db_prefix = tuple(db_prefix)
-        db_names = [n for n in h5_names if n.startswith(db_prefix)]
-        assert len(db_names)
-    elif db_list:
-        db_names = [
-            n for n, _ in parse_image_lists_with_intrinsics(db_list)]
-    elif db_model:
+    if db_model:
         images = read_images_binary(db_model / 'images.bin')
         db_names = [i.name for i in images.values()]
     else:
-        raise ValueError('Provide either prefixes of DB names, or path to '
-                         'lists of DB images, or path to a COLMAP model.')
-
-    if query_prefix or query_prefix == "":
-        if not isinstance(query_prefix, str):
-            query_prefix = tuple(query_prefix)
-        query_names = [n for n in h5_names if n.startswith(query_prefix)]
-        assert len(query_names)
-    elif query_list:
-        query_names = [
-            n for n, _ in parse_image_lists_with_intrinsics(query_list)]
-    else:
-        raise ValueError('Provide either prefixes of query names, or path to '
-                         'lists of query images.')
+        db_names = parse_names(db_prefix, db_list, db_names_h5)
+    if len(db_names) == 0:
+        raise ValueError('Could not find any database image.')
+    query_names = parse_names(query_prefix, query_list, query_names_h5)
 
     device = 'cuda' if torch.cuda.is_available() else 'cpu'
     db_desc = get_descriptors(db_names, db_descriptors, name2db)
@@ -125,5 +113,6 @@ if __name__ == "__main__":
     parser.add_argument('--db_prefix', type=str, nargs='+')
     parser.add_argument('--db_list', type=Path)
     parser.add_argument('--db_model', type=Path)
+    parser.add_argument('--db_descriptors', type=Path)
     args = parser.parse_args()
     main(**args.__dict__)
